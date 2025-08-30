3555fea0b168fb8da64474e307ca45ff0e0cb339
LiorRey <lior_rey@hotmail.com>
2025-08-31 02:23:50 +0300

    Add navigation links and AboutUs component

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,5 +1,6 @@
 import { AppHeader } from "./cmps/AppHeader.jsx"
 import { HomePage } from "./cmps/HomePage.jsx"
+import { AboutUs } from "./cmps/AboutUs.jsx"
 
 const Router = ReactRouterDOM.HashRouter
 const { Routes, Route, Navigate } = ReactRouterDOM
@@ -13,7 +14,7 @@ export function RootCmp() {
           <Routes>
             <Route path="/" element={<Navigate to="/home" />} />
             <Route path="/home" element={<HomePage />} />
-            {/* <Route path="/about" element={<AboutUs />} /> */}
+            <Route path="/about" element={<AboutUs />} />
             {/* <Route path="/book" element={<BookIndex />} /> */}
             {/* <Route path="/book/:bookId" element={<BookDetails />} /> */}
             {/* <Route path="/book/edit" element={<BookEdit />} /> */}
---
+++ cmps/AboutUs.jsx
@@ -0,0 +1,15 @@
+const { useState, useEffect } = React
+
+export function AboutUs() {
+  return (
+    <section className="about-us">
+      <h2>About Miss Books and us... 🤓</h2>
+      <span>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis ut
+        fugit id libero, doloremque exercitationem architecto, amet suscipit
+        ratione consectetur nesciunt officiis aperiam iusto! Rem eius blanditiis
+        itaque. Nihil, natus?
+      </span>
+    </section>
+  )
+}
