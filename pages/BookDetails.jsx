9716fe74d92f6319c5fc5afda81c96166f22ba46
LiorRey <lior_rey@hotmail.com>
2025-09-05 04:29:11 +0300

    Fix incorrect import of LongTxt in BookDetails

--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -1,4 +1,4 @@
-import { LongTxt } from "./LongTxt.jsx"
+import { LongTxt } from "../cmps/LongTxt.jsx"
 import { bookService } from "../services/book.service.js"
 
 const { useState, useEffect } = React
